4adf232da78b08d83f2fe2aa98b865d6cd399989
SHASHINI-0278 <shashinibscit@gmail.com>
2026-02-18 09:58:35 +0530

    changes for mobile response

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -62,8 +62,6 @@ function DashBoard() {
             onChange={(e) => setSearch(e.target.value)}
         />
 
-      </div>
-
         <select
             className="p-2 border rounded ml-4"
             value={month}
@@ -84,7 +82,7 @@ function DashBoard() {
             <option value="desc">Revenue: High → Low</option>
         </select>
 
-
+      </div>
 
       <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
